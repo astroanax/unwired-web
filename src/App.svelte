4de43cc4eaed8b2053547124e463d070344c786f
Rehan Tadpatri <astroanax@outlook.com>
2024-08-20 21:59:38 +0530

    now themeing :evil:

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,49 @@
 <svelte:options immutable={true}/>
 
 <script>
+	import Header from './lib/header.svelte';
+	import Footer from './lib/footer.svelte';
 	import { Slidy } from '@slidy/svelte'
-	import pkg from '@slidy/svelte/package.json'
+	import { flip } from "@slidy/animation";
 
 	import { getPhotos } from './lib/photos.js'
 
 	let index = 4, position = 0, limit = 6
 </script>
 
-<header>
-	<h1>{pkg.name}@{pkg.version}</h1>
-	<p>index: [{index}] position: {Math.trunc(position)}px</p>
-</header>
+<svelte:head>
+	       <link rel="preconnect" href="https://fonts.googleapis.com">
+	       <link rel="preconnect" href="https://fonts.gstatic.com">
+	       <link href="https://fonts.googleapis.com/css2?family=Anek+Devanagari:wght@100..800&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
+	       <title>
+	               Team Unwired - Projects
+			       </title>
+</svelte:head>
 
+<Header />
 <main>
+	<h2> Team Unwired's Project over the years</h2>
 	{#await getPhotos(limit) then slides}
 		<Slidy
 				{slides}
 				bind:index
 				bind:position
 				snap="center"
+				animation={flip}
 				thumbnail
 		/>
 		{/await}
 </main>
+<Footer/>
 
 <style>
 	@import url('https://unpkg.com/@slidy/svelte/dist/slidy.css');
-	main {
-		height: 75%
+	h2{
+		text-align: center;
+	}
+	main{
+		margin-bottom: auto;
+		flex-grow: 1;
+		width: 100%;
 	}
 </style>
