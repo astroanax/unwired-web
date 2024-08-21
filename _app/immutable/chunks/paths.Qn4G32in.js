c2faa005dc22b495e80274639f045395f6548bb9
Rehan Tadpatri <astroanax@outlook.com>
2024-08-21 23:02:22 +0530

    Updates

---
+++ _app/immutable/chunks/paths.Qn4G32in.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_3oyfeb)==null?void 0:s.base)??"/unwired-web";var e;const t=((e=globalThis.__sveltekit_3oyfeb)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.B1f4MzOj.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.zMftxnGQ.js";export{t as start};
